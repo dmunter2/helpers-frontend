d2fa1b5368b286eaa59417cfb886b706e226856a
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2020-01-29 21:15:16 -0800

    style

---
+++ src/components/Likes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Likes(){
+
+
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+
+export default Likes;
+
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,29 +3,76 @@ import { connect } from 'react-redux'
 import { addUserPost } from '../actions/postAction'
 import PostCard from './PostCard';
 import Style from 'styled-components';
-
+import Likes from './Likes'
 
 const DIV = Style.div`
 background-color: #F5F5F5;
-width: 80%;
+width: 60%;
 height: 1000px;
 box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 border-radius: 5px;
+display: flex;
+flex-direction: column;
 
 `
 const MAIN = Style.div`
 display: flex;
+flex-direction: row;
+// justify-content: center;
+justify-content: space-around;
+// align-items: center;
+padding-left: 1%;
+padding-right: 1%;
+`
+const DIV2 = Style.div`
+// background-color: blue;
+border-radius: 5px;
+height: 140px;
+margin: 3%;
+width: 300px;
+display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-`
-const DIV2 = Style.div`
-background-color: blue;
-border-radius: 5px 5px 0 0;
-height: 60px;
 
 
 `
+const DIV3 = Style.div``
+
+const INPUT = Style.textarea`
+max-width: 250px;
+width: 250px;
+max-height: 80px;
+border: none;
+border-radius: 5px;
+background-color: #e4e4e2;
+padding: 2%;
+font-family: 'Open Sans', sans-serif;
+font-size: .8rem;
+`
+const HOLDER = Style.div`
+width: 100%;
+text-align: end;
+`
+
+const BUTTON = Style.button`
+border: 1px solid  #1f2020;
+background-color: #1f2020;
+height: 30px;
+border-radius: 5px;
+width: 80px;
+color: white;
+margin: 10px 25px 0 0;
+
+`
+const DIV4 = Style.div`
+width: 35%;
+background-color: #F5F5F5;
+height: 1000px;
+box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+border-radius: 5px;
+`
+
 
 
 class PostForm extends React.Component {
@@ -48,8 +95,8 @@ class PostForm extends React.Component {
                 <DIV>
 
                     <DIV2>
-                        <input
-                            rows="5" cols="50"
+                        <INPUT
+                            rows="5"
                             type='textarea'
                             placeholder='Post'
                             value={this.state.newPost}
@@ -59,18 +106,26 @@ class PostForm extends React.Component {
 
                         />
 
-
-                        <button onClick={() => this.props.addUserPost(this.state.newPost)}>Add Post</button>
+                    <HOLDER>
+                            <BUTTON onClick={() => this.props.addUserPost(this.state.newPost)}>Add Post</BUTTON>
+                    </HOLDER>
                     </DIV2>
+
+                    <DIV3>
+                        {this.props.posts.map((post, index) => {
+                            return (
+                                <PostCard post={post.title} />
+                            )
+
+                        })}
+                    </DIV3>
                              
 
-                                {this.props.posts.map((post, index) => {
-                                    return (
-                                        <PostCard post={post.title} />
-                                    )
-
-                                })}
+                           
                 </DIV>
+                <DIV4>
+                    <Likes />
+                </DIV4>
 
             </MAIN>
 
